117ec846b0584341935b37364e0f54455db0a426
Olga Markevich <pyrkoolga@gmail.com>
2019-04-02 20:29:32 +0300

    правки форма обратной связи

--- src/scripts/forms.js
+++ src/scripts/forms.js
@@ -4,6 +4,18 @@ Vue.use(Vuelidate);
 import { required, email } from 'vuelidate/lib/validators'
 const axios = require('axios');
 
+Vue.component('modal', {
+  template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box"><slot></slot></div>
+      </div>
+      <button class="modal-close" @click="$emit('close')"></button>
+    </div>
+  `
+});
+
 new Vue({
     el:"#fb-component",
     data: {
@@ -37,13 +49,16 @@ new Vue({
         });
       },
       submitFb() {
-        axios.post('https://webdev-api.loftschool.com/register', {
+        axios.post('https://webdev-api.loftschool.com/sendmail', {
           name: this.fbName,
+          to: "pyrkoolga@gmail.com",
           mail: this.fbMail,
-          msg: this.fbMsg
+          comment: this.fbMsg,
+          phone: "111"
         })
         .then(function (response) {
           alert('Сообщение отправлено!');
+          
         })
         .catch(function (error) {
           alert('Не удалось отправить сообщение!');
