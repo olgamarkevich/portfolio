1d01906496b039f4efb35e675c0baa11b4648de6
Olga Markevich <pyrkoolga@gmail.com>
2019-04-07 20:45:15 +0300

    админка spa v.0.0.1

---
+++ src/admin/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+import categories from './modules/categories'
+import skills from './modules/skills'
+
+export default new Vuex.Store({
+    modules:{
+        categories,
+        skills
+    }
+})
